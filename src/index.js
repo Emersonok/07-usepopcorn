1518b731c0e86ac76de5846d963243d6763721a4
Emerson Okorie <emersonok52@gmail.com>
2023-11-11 07:00:54 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
+import App from "./App2.js";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating defaultRating={1} /> <br />
+    {/* <StarRating defaultRating={1} /> <br />
     <StarRating
       color="blue"
       size={43}
@@ -23,6 +24,7 @@ root.render(
       className="test"
       maxRating={10}
       messages={["1", "2", "3", "4", "5", "6", "7", "8", "now"]}
-    />
+    /> */}
+    <App />
   </React.StrictMode>
 );
